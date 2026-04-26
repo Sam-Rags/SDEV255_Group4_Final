dbe0156a0960a23c93f85dbace3254427ad77c7e
Sam-Rags <168376297+Sam-Rags@users.noreply.github.com>
2026-04-26 09:14:00 -0400

    render update test

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+addEventListener("DOMContentLoaded", async function() {
+    const response = await fetch("https://sdev255-group4-final.onrender.com/api/courses")
+    const courses = await response.json()
+
+    let html = ""
+    for (let course of courses) {
+        let courseID = course._id
+        html +=`<li>${course.courseNumber} - ${course.courseName} - <a href="course-details.html?id=${courseID}">Details</a> - <a href="edit-course.html?id=${courseID}">Edit</a> </li>`
+    }
+
+    document.querySelector("#courseList").innerHTML = html;
+})
+//
